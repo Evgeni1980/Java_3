8bcc7ff12f0ddac52676bb85a34d3fc9aa6942c2
Evgeni <Kremeni@gmail.com>
2022-01-27 21:14:19 +0300

    Добавленно ДЗ 1

---
+++ src/Geekbrains/HomeWork/Orange.java
@@ -0,0 +1,18 @@
+package Geekbrains.HomeWork;
+
+public class Orange extends Fruit{
+
+    public Orange(float weight) {
+        super(weight);
+    }
+
+    public Orange() {
+        super(1.5F);
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+        // return "апельсины";
+    }
+}
